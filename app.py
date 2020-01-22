d0336bfdb849d0d0b111ebc037d2d50878bcb660
Ray <12276029+tesoph@users.noreply.github.com>
2020-01-22 22:54:50 +0000

    Add helpers.py from cs50 finance

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_pymongo import PyMongo
+from helpers import apology, login_required
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
@@ -11,16 +16,33 @@ print(f'hi, {a}')
 print(f'hi, {b}')
 c=os.environ['MONGO_URI']
 print(c)
+'''
+from cs50
+'''
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+#the location to store information about the session
+app.config["SESSION_TYPE"] = "filesystem"
+#enable sessions for this particular flask web application
+Session(app)
 
-mongo = PyMongo(app)
+MONGO_URI=os.environ['MONGO_URI']
+MONGO_DBNAME=os.environ['MONGO_DBNAME']
 
-@app.route('/')
-def hello():
-    return 'Hello World a...again'
+#mongo = PyMongo(app)
+
+client = MongoClient(MONGO_URI)
+db= client.ms3
+
+@app.route("/")
+#@login_required
+def index():
+    """index page"""
+    return apology("TODO")
 
 
 if __name__ == '__main__':
-    
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
             debug=True)
