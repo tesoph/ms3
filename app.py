0c84492c09223e11b4ffe0dfc2629d4ee09de371
Ray <12276029+tesoph@users.noreply.github.com>
2020-01-23 20:41:52 +0000

    Update templates, add register() to app.py

--- app.py
+++ app.py
@@ -25,7 +25,55 @@ MONGO_DBNAME=os.environ['MONGO_DBNAME']
 
 client = MongoClient(MONGO_URI)
 db= client.ms3
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
 
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # check password confirmation
+        if not request.form.get('password') == request.form.get('confirmation'):
+            return apology("passwords don't match", 403)
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        
+        
+        user=request.form.to_dict()
+        alreadyExists = db.users.find_one({"username": user['username']})
+
+     
+        del user['confirmation']
+        plain = request.form.get("password")
+        user['password'] = generate_password_hash(plain, method='pbkdf2:sha256', salt_length=8)
+
+        if alreadyExists:
+            return apology("Username already exists", 400)
+        if not alreadyExists:
+            db.users.insert_one(user)
+     
+
+        # log user in
+        '''
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                        username=request.form.get("username"))'''
+        #db.users.find_one({"username": session['user']})
+        # Remember which user has logged in
+        #session["user_id"] = rows[0]["id"]
+        session['user_id'] = (db.users.find_one({"username": user['username']}))['_id']
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+        
 if __name__ == '__main__':
     
     app.run(host=os.environ.get('IP', '0.0.0.0'),
