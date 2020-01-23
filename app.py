2072f2a0a55361320f24f844c8ace44661567c21
Ray <12276029+tesoph@users.noreply.github.com>
2020-01-23 00:07:11 +0000

    Add and update login function from cs50-finance

--- app.py
+++ app.py
@@ -83,14 +83,56 @@ def register():
         #db.users.find_one({"username": session['user']})
         # Remember which user has logged in
         #session["user_id"] = rows[0]["id"]
-        #session['user_id'] = (db.users.find_one({"username": user['username']}))['id']
+        session['user_id'] = (db.users.find_one({"username": user['username']}))['_id']
         # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
-        
+
+
+'''
+login() adapted from c50 finance
+'''
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        '''
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))'''
+        user = db.users.find_one({"username": request.form.get("username")})
+
+        # Ensure username exists and password is correct
+        if not user or not check_password_hash(user['password'], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session['user_id'] = (db.users.find_one({"username": user['username']}))['_id']
+       #session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
